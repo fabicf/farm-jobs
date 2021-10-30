de46001caebbd7182bf779adba4edbe1bb6b2304
fabicf <fabiolacezarfaria@gmail.com>
2021-10-30 12:16:11 -0400

    Initial commit

---
+++ backend/src/app.js
@@ -0,0 +1,15 @@
+const cors = require('cors');
+const express = require('express')
+const app = express()
+const port = 3000
+const routes = require('./routes');
+
+
+app.use(express.json());
+app.use(cors()); //app express use cors
+app.use(routes);
+
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
